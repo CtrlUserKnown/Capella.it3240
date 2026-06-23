6d2395c9d37511a383cc535f56d0ff4b21d6626e
CtrlUserKnown <Alvarez.christian46@gmail.com>
2026-06-23 18:07:07 -0500

    refactor: rename toolNames to tools across weeks 6-9

--- week9/app.js
+++ week9/app.js
@@ -3,21 +3,21 @@
 // Date: 06/14/2026
 
 // Array of open-source tool names developed by FourSides Studio
-let toolNames = ["FractalForge", "ChromaEngine", "Timeline Weaver", "VectorForge", "ShaderSync"];
+let tools = ["FractalForge", "ChromaEngine", "Timeline Weaver", "VectorForge", "ShaderSync"];
 
 // Log the array to confirm its contents
-console.log("Array of tool names:", toolNames);
-console.log("Number of tools in array:", toolNames.length);
+console.log("Array of tool names:", tools);
+console.log("Number of tools in array:", tools.length);
 
-// Builds an HTML unordered list from the toolNames array
+// Builds an HTML unordered list from the tools array
 function buildToolList() {
     // Variable to hold the assembled HTML string for the list
     let listCode = "<ul class=\"tool-list\">";
     // Loop through each item in the array and wrap it in list item tags
-    for (let i = 0; i < toolNames.length; i++) {
-        listCode += "<li>" + toolNames[i] + "</li>";
+    for (let i = 0; i < tools.length; i++) {
+        listCode += "<li>" + tools[i] + "</li>";
         // Log each item as it is added to the list
-        console.log("Added tool to list:", toolNames[i]);
+        console.log("Added tool to list:", tools[i]);
     }
     listCode += "</ul>";
     // Insert the completed list HTML into the paragraph with id "list"
